758f0a0fe67fc72377202cc5e2bf23136a0a549b
Thobaniw <Gamathobani9@gmail.com>
2023-08-20 17:30:23 +0200

    Task 9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,12 +1,13 @@
 -- This command create a table with multiple rows
-CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
+CREATE TABLE IF NOT EXISTS hbtn_test_db_9.second_table (
     id INT,
     name VARCHAR(256),
     score INT
 );
 
-INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES
+INSERT INTO hbtn_test_db_9.second_table (id, name, score) VALUES
 (1, 'John', 10),
 (2, 'Alex', 3),
 (3, 'Bob', 14),
 (4, 'George', 8);
+
