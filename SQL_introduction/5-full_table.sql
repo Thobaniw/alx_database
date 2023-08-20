772a7b47a56ba843764f408c534feed61f6e8efa
Thobaniw <Gamathobani9@gmail.com>
2023-08-20 16:26:32 +0200

    Task 5

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- This command show the description of a table
+SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'first_table' AND TABLE_SCHEMA = 'hbtn_0c_0';
