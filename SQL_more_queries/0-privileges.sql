8eb0f60b57f114adcc7a58ea3f7ad3b9dfa848f1
Thobaniw <Gamathobani9@gmail.com>
2023-08-21 03:55:57 +0200

    Task 0

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- This command show the list of privileges for user_0_od_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- This command show the list of privileges for user_od_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
