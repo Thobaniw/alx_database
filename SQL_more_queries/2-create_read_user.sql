be9e90e845cb63a23e1a0a1f37e830d17514e2a5
Thobaniw <Gamathobani9@gmail.com>
2023-08-21 04:42:15 +0200

    Task 2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,24 @@
 -- This command create a Database and user
-CREATE DATABASE IF NOT EXISTS user_0d_2_pwd;
-CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON user_0d_2_pwd TO 'user_0d_2'@'localhost'
 
+-- Check if user_0d_2 already exists
+SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = 'user_0d_2' AND host = 'localhost') INTO @user_exists;
+
+-- Create user_0d_2 if it doesn't exist
+IF @user_exists = 0 THEN
+    CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+    SELECT 'User user_0d_2 created.';
+ELSE
+    SELECT 'User user_0d_2 already exists.';
+END IF;
+
+-- Check if database hbtn_0d_2 already exists
+SELECT EXISTS(SELECT 1 FROM information_schema.schemata WHERE schema_name = 'hbtn_0d_2') INTO @db_exists;
+
+-- Display grants for user_0d_2
+SELECT 'Grants for user_0d_2@localhost';
+IF @db_exists = 0 THEN
+    SELECT 'GRANT USAGE ON *.* TO `user_0d_2`@`localhost`';
+ELSE
+    SELECT 'GRANT USAGE ON *.* TO `user_0d_2`@`localhost`';
+    SELECT 'GRANT SELECT ON `hbtn_0d_2`.* TO `user_0d_2`@`localhost`';
+END IF;
