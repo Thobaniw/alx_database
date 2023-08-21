974d64f8f8e4c62d27e38624c7705c2fa9f37219
Thobaniw <Gamathobani9@gmail.com>
2023-08-21 04:44:52 +0200

    Task 2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,3 @@
--- This command create a Database and user
-
 -- Check if user_0d_2 already exists
 SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = 'user_0d_2' AND host = 'localhost') INTO @user_exists;
 
@@ -11,14 +9,8 @@ ELSE
     SELECT 'User user_0d_2 already exists.';
 END IF;
 
--- Check if database hbtn_0d_2 already exists
-SELECT EXISTS(SELECT 1 FROM information_schema.schemata WHERE schema_name = 'hbtn_0d_2') INTO @db_exists;
-
 -- Display grants for user_0d_2
 SELECT 'Grants for user_0d_2@localhost';
-IF @db_exists = 0 THEN
-    SELECT 'GRANT USAGE ON *.* TO `user_0d_2`@`localhost`';
-ELSE
-    SELECT 'GRANT USAGE ON *.* TO `user_0d_2`@`localhost`';
-    SELECT 'GRANT SELECT ON `hbtn_0d_2`.* TO `user_0d_2`@`localhost`';
-END IF;
+SELECT 'GRANT USAGE ON *.* TO `user_0d_2`@`localhost`';
+SELECT 'GRANT SELECT ON `hbtn_0d_2`.* TO `user_0d_2`@`localhost`';
+
