5eef1c9e1b65ce896ea09b180c0abc5afec7a696
Thobaniw <Gamathobani9@gmail.com>
2023-08-21 04:33:06 +0200

    Task2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- This command create a Database and user
-CREATE DATABASE user_0d_2_pwd;
+CREATE DATABASE IF NOT EXISTS user_0d_2_pwd;
 CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON user_0d_2_pwd TO 'user_0d_2'@'localhost'
 
