a59f784aefc60ee28d7a3bdc2acc7f4158b14ab3
Thobaniw <Gamathobani9@gmail.com>
2023-08-22 06:11:09 +0200

    Task 2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,26 +1,11 @@
--- Check if database hbtn_0d_2 already exists
-SELECT EXISTS(SELECT 1 FROM information_schema.schemata WHERE schema_name = 'hbtn_0d_2') INTO @db_exists;
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
--- Create database hbtn_0d_2 if it doesn't exist
-IF @db_exists = 0 THEN
-    CREATE DATABASE hbtn_0d_2;
-    SELECT 'Database hbtn_0d_2 created.';
-ELSE
-    SELECT 'Database hbtn_0d_2 already exists.';
-END IF;
+-- Create or update the user
+CREATE OR REPLACE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
--- Check if user_0d_2 already exists
-SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = 'user_0d_2' AND host = 'localhost') INTO @user_exists;
-
--- Create user_0d_2 if it doesn't exist
-IF @user_exists = 0 THEN
-    CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-    GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
-    FLUSH PRIVILEGES;
-    SELECT 'User user_0d_2 created and granted SELECT privilege on hbtn_0d_2 database.';
-ELSE
-    SELECT 'User user_0d_2 already exists.';
-END IF;
+-- Grant SELECT privilege to the user on the database
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 
 
 
