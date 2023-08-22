f0ce00e71737ed5fcce479a38545cc3f8d45b276
Thobaniw <Gamathobani9@gmail.com>
2023-08-22 06:14:29 +0200

    Task 2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -2,9 +2,12 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 -- Create or update the user
-CREATE OR REPLACE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
--- Grant SELECT privilege to the user on the database
+-- Grant USAGE privilege to the user on all databases
+GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
+
+-- Grant SELECT privilege to the user on the hbtn_0d_2 database
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 
 
