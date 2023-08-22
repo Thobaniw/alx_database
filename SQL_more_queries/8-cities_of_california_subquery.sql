3beed97dd4196d97b82ac566c1dc5c9675b125d0
Thobaniw <Gamathobani9@gmail.com>
2023-08-22 07:26:28 +0200

    Task 8

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,12 @@
+-- This command create a list of cities on the hbtn_0d_usa Database
+-- Use the database
+USE hbtn_0d_usa;
+
+-- Get the state_id for California
+SET @california_state_id = (SELECT id FROM states WHERE name = 'California');
+
+-- List cities of California
+SELECT cities.id, cities.name
+FROM cities
+WHERE cities.state_id = @california_state_id
+ORDER BY cities.id ASC;
