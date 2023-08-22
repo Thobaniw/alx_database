9c2d157c9cb62b75a23dd8cf7afe09c539e30d0b
Thobaniw <Gamathobani9@gmail.com>
2023-08-22 07:15:16 +0200

    Task 7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,6 +1,20 @@
--- This command create a Database hbtn_0d_usa
+-- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
--- THis command use the created Database 
+
+-- Use the database
 USE hbtn_0d_usa;
--- This command create a table
-CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, state_id INT NOT NULL, name VARCHAR(256) NOT NULL, FOREIGN KEY (state_id) REFERENCES state (id)); 
+
+-- Create the cities table if it doesn't exist
+CREATE TABLE IF NOT EXISTS cities (
+    id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);
+
+-- Insert data into cities table
+INSERT INTO cities (state_id, name) VALUES
+(1, 'San Francisco'),
+(1, 'San Diego'),
+(2, 'Page'),
+(2, 'Phoenix');
